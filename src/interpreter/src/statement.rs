e23f401a22a236b470f3a9593e5816766467490d
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2025-12-02 03:55:00 -0600

    upsy

--- src/interpreter/src/statement.rs
+++ src/interpreter/src/statement.rs
@@ -14,59 +14,60 @@ impl Interpreter {
         return new_ctx.borrow_mut().eval_statement(ctx, lines);
     }
 
-    pub fn eval_if_statement(&mut self, ctx: &Ctx, if_stmt: &If) -> ReasonsForStopping {
+    pub fn eval_if_statement(&mut self, ctx: &Ctx, if_stmt: &If) -> (Types, ReasonsForStopping) {
         let condition = self.eval_expression(ctx, &if_stmt.condition, if_stmt.condition_bool);
         if condition.to_number() > 0.0 {
             let new_ctx = self.new_context(ctx);
-            let (_, reason) = new_ctx
+            return new_ctx
                 .borrow_mut()
                 .eval_statement(&new_ctx, &if_stmt.if_then);
-            return reason;
         }
         for elif in &if_stmt.elif_then {
             let condition = self.eval_expression(ctx, &elif.condition, elif.condition_bool);
             if condition.to_number() > 0.0 {
                 let new_ctx = self.new_context(ctx);
-                let (_, reason) = new_ctx.borrow_mut().eval_statement(&new_ctx, &elif.then);
-                return reason;
+                return new_ctx.borrow_mut().eval_statement(&new_ctx, &elif.then);
             }
         }
         let new_ctx = self.new_context(ctx);
-        let (_, reason) = new_ctx
+        return new_ctx
             .borrow_mut()
             .eval_statement(&new_ctx, &if_stmt.else_then);
-        return reason;
     }
-    pub fn eval_while_loop(&mut self, ctx: &Ctx, while_loop: &WhileLoop) -> ReasonsForStopping {
+    pub fn eval_while_loop(
+        &mut self,
+        ctx: &Ctx,
+        while_loop: &WhileLoop,
+    ) -> (Types, ReasonsForStopping) {
         let new_ctx = self.new_context(ctx);
-        let mut interpreter= new_ctx.borrow_mut();
+        let mut interpreter = new_ctx.borrow_mut();
         let mut condition = self.eval_expression(ctx, &while_loop.condition, while_loop.is_bool);
         while condition.to_number() > 0.0 {
-            let (_, reason) = interpreter.eval_statement(ctx, &while_loop.body);
+            let (out, reason) = interpreter.eval_statement(ctx, &while_loop.body);
             match &reason {
                 ReasonsForStopping::Finished | ReasonsForStopping::ContinueStatement => {}
-                _ => return reason,
+                _ => return (out, reason),
             }
             condition = self.eval_expression(ctx, &while_loop.condition, while_loop.is_bool);
         }
-        return ReasonsForStopping::Finished;
+        return (Types::Number(0.0), ReasonsForStopping::Finished);
     }
-    pub fn eval_for_loop(&mut self, ctx: &Ctx, for_loop: &ForLoop) -> ReasonsForStopping {
+    pub fn eval_for_loop(&mut self, ctx: &Ctx, for_loop: &ForLoop) -> (Types, ReasonsForStopping) {
         let new_ctx = self.new_context(ctx);
-        let mut interpreter=new_ctx.borrow_mut();
+        let mut interpreter = new_ctx.borrow_mut();
         // first once
         self.eval_expression(ctx, &for_loop.init, false);
         let condition = self.eval_expression(ctx, &for_loop.condition, for_loop.is_bool);
         while condition.to_number() > 0.0 {
-            let (_, reason) =interpreter.eval_statement(ctx, &for_loop.body);
+            let (out, reason) = interpreter.eval_statement(ctx, &for_loop.body);
             match &reason {
                 ReasonsForStopping::Finished | ReasonsForStopping::ContinueStatement => {}
-                _ => return reason,
+                _ => return (out, reason),
             }
             self.eval_expression(ctx, &for_loop.condition, for_loop.is_bool);
             self.eval_expression(ctx, &for_loop.increment, false);
         }
-        return ReasonsForStopping::Finished;
+        return (Types::Number(0.0), ReasonsForStopping::Finished);
     }
 
     pub fn eval_statement(&mut self, ctx: &Ctx, lines: &Vec<Stmt>) -> (Types, ReasonsForStopping) {
@@ -117,22 +118,26 @@ impl Interpreter {
                     }
                 }
                 Stmt::If(if_stmt) => {
-                    let reason = current_scope.eval_if_statement(ctx, if_stmt);
+                    let (exit, reason) = current_scope.eval_if_statement(ctx, if_stmt);
                     match &reason {
                         ReasonsForStopping::Finished => {}
-                        _ => return (out, reason),
+                        _ => return (exit, reason),
                     }
                 }
                 Stmt::WhileLoop(while_loop) => {
-                    let reason = current_scope.eval_while_loop(ctx, while_loop);
+                    let (exit, reason) = current_scope.eval_while_loop(ctx, while_loop);
                     match &reason {
-                        ReasonsForStopping::Finished
-                        | ReasonsForStopping::BreakStatement
-                        | ReasonsForStopping::ContinueStatement => {}
-                        _ => return (out, reason),
+                        ReasonsForStopping::ReturnStatement => return (exit, reason),
+                        _ => {}
                     }
                 }
-                Stmt::ForLoop(_for_loop) => todo!("implement a for loop evaluator"),
+                Stmt::ForLoop(for_loop) => {
+                    let (exit,reason) = current_scope.eval_for_loop(ctx, for_loop);
+                    match &reason {
+                        ReasonsForStopping::ReturnStatement => return (exit, reason),
+                        _ => {}
+                    }
+                }
             }
             println!("{:?}", out);
 
